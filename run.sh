05398e55b792db1e75f0f3c9f70a747f0dbe4e9c
abhimanyu <manyu.abhi17@gmail.com>
2022-08-29 12:26:35 +0530

    Added Readme

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ mvn clean install -DskipTests assembly:single -q
 # echo "INPUT 1"
 # java -jar target/geektrust.jar sample_input/input1.txt
 # echo "INPUT 2"
-# java -jar target/geektrust.jar sample_input/input2.txt
+java -jar target/geektrust.jar sample_input/input2.txt
 # echo "INPUT 3"
 # java -jar target/geektrust.jar sample_input/input3.txt
 # echo "INPUT 4"
 # java -jar target/geektrust.jar sample_input/input4.txt
 # echo "INPUT 5"
-java -jar target/geektrust.jar sample_input/input5.txt
+# java -jar target/geektrust.jar sample_input/input5.txt
 # echo "INPUT 6"
 # java -jar target/geektrust.jar sample_input/input6.txt
